ad1d86273eb7d2d02bc26d97ffae00b0d40b4a4a
Chris Maresca <chris@chrismaresca.dev>
2025-02-20 10:45:00 -0500

    trying to fix blur error on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-background">
       <Hero />
-      {/* <div className="mt-16 motion-opacity-in-0 motion-blur-in-md motion-duration-[800ms] motion-delay-[1200ms] motion-ease-in-out">
-        <LogoTicker />
-      </div> */}
+
       <Benefits />
       <Testimonials />
       <CaseProcess />
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -25,16 +25,16 @@ export default function Hero() {
       <section className="container overflow-x-hidden relative pt-24 md:pt-32">
         <div className="grid gap-8 lg:grid-cols-2 lg:gap-12">
           <div className="flex flex-col justify-center space-y-8">
-            <div className="space-y-6 motion-opacity-in-0 motion-blur-in-md motion-duration-[800ms] motion-delay-300 motion-ease-spring-smooth">
+            <div className="space-y-6 motion-opacity-in-0 motion-duration-[800ms] motion-delay-300 motion-ease-spring-smooth">
               <h1 className="text-5xl font-bold tracking-tighter sm:text-6xl md:text-7xl">{heroSection.title}</h1>
               <p className="max-w-[600px] text-lg text-muted-foreground sm:text-xl">{heroSection.description}</p>
             </div>
 
-            <div className="flex max-w-full flex-col gap-4 sm:flex-row motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md motion-duration-[800ms] motion-delay-700 motion-ease-spring-smooth">
+            <div className="flex max-w-full flex-col gap-4 sm:flex-row motion-opacity-in-0 motion-translate-y-in-100 md:motion-blur-in-md motion-duration-[800ms] motion-delay-700 motion-ease-spring-smooth">
               <div className="hidden sm:block">
                 <MotionButtonWithCustomIcon size="lg" textOptions={{ textOptions: [{ text: heroSection.mainCTA, href: CONTACT_US_CALL_LINK }], blank: true }} fromColor="bg-primary" toColor="bg-black" switchTextColor={false} />
               </div>
-              <Link href={CONTACT_US_CALL_LINK} className="w-full sm:hidden">
+              <Link href={CONTACT_US_CALL_LINK} target="_blank" className="w-full sm:hidden">
                 <Button size="lg" variant="default" className="flex items-center w-full">
                   {heroSection.mainCTA}
                   <ArrowRight className="ml-2 h-4 w-4" />
@@ -46,7 +46,7 @@ export default function Hero() {
               </Button>
             </div>
 
-            <div className="flex items-center space-x-4 text-sm motion-opacity-in-0 motion-blur-in-md motion-duration-[800ms] motion-delay-[1000ms] motion-ease-spring-smooth">
+            <div className="flex items-center space-x-4 text-sm motion-opacity-in-0 md:motion-blur-in-md motion-duration-[800ms] motion-delay-[1000ms] motion-ease-spring-smooth">
               <div className="flex -space-x-1.5">
                 {[...Array(4)].map((_, i) => (
                   <Avatar key={i} className="h-8 w-8">
@@ -59,11 +59,11 @@ export default function Hero() {
             </div>
           </div>
 
-          <div className="motion-opacity-in-0 motion-blur-in-md motion-duration-[800ms] motion-delay-300 motion-ease-spring-smooth">
+          <div className="motion-opacity-in-0 md:motion-blur-in-md motion-duration-[800ms] motion-delay-300 motion-ease-spring-smooth">
             <Video />
           </div>
         </div>
-        <div className="mt-16 motion-opacity-in-0 motion-blur-in-md motion-duration-[800ms] motion-delay-[1200ms] motion-ease-in-out">
+        <div className="mt-16 motion-opacity-in-0 md:motion-blur-in-md motion-duration-[800ms] motion-delay-[1200ms] motion-ease-in-out">
           <LogoTicker />
         </div>
       </section>
